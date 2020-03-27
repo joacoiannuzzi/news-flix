941be7a5b78a4f501b6aeb1d94b8d3a7768da6fa
Pedro Gardelliano <pedro.gardelliano@ing.austral.edu.ar>
2020-03-27 12:48:52 -0300

    Added user registration, missing login still.

--- src/main/java/com/lab1/service/Signup.java
+++ src/main/java/com/lab1/service/Signup.java
@@ -2,6 +2,7 @@ package com.lab1.service;
 
 import com.lab1.entity.Users;
 import com.lab1.model.User;
+import org.omg.CORBA.SystemException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,10 +11,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet("/signup.html")
+import static com.lab1.util.LangUtils.*;
+import static com.lab1.util.LangUtils.notEmpty;
+import static java.lang.String.join;
+
+@WebServlet("/signup.do")
 public class Signup extends HttpServlet {
 
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    req.getSession().invalidate();
+
+    final RequestDispatcher view = req.getRequestDispatcher("/signup.html");
+
+    view.forward(req,resp);
+  }
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     final User user = new User();
@@ -24,7 +38,12 @@ public class Signup extends HttpServlet {
     user.setPassword(req.getParameter("password"));
     user.setActive(true);
 
-    Users.persist(user);
+
+    try {
+      Users.persist(user);
+    } catch(SystemException e){
+      e.printStackTrace();
+    }
 
     final RequestDispatcher view = req.getRequestDispatcher("login.html");
 
